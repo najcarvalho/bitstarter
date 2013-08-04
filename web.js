754ea3dd62dcb0ab9ab079b32172b71501d6de29
najcarvalho <najcarvalho2003@yahoo.com>
2013-08-04 07:05:22 +0000

    play

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var buffer = new Buffer(100);
   response.send('Neville says -- Hello Web! -- latest realzzzz');
 });
 

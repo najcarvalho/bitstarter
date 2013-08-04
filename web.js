e2e6764d4dc608355a19dcc9a4f76afde8e8012d
najcarvalho <najcarvalho2003@yahoo.com>
2013-08-04 07:22:31 +0000

    play6

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buffer = new Buffer(100);
-  buffer.write("Sam says Hello Web!", "utf-8");
+    buffer.write(fs.readFileSync("index.html"), "utf-8");
     response.send(buffer.toString("utf-8", 0, 19));
   response.send('Neville says -- Hello Web! -- latest realaay');
 });
